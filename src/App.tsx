e58ff550da6259e6aa9af6c2662a812b5f982024
Juhász Csenge <csenge799@gmail.com>
2023-11-16 22:17:29 +0100

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './Page/Home/HomePage';
