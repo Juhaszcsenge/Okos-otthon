51f3e092a294c964eaa1e18a78540608f65a94d0
Juhász Csenge <csenge799@gmail.com>
2023-11-29 01:14:35 +0100

    footer

--- src/Component/Footer.tsx
+++ src/Component/Footer.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import './Footer.css'; 
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div className="footer-container" style={{background: "var(--dark-background, #181818)", color:"white", marginTop:"50px"}}>
+    <section>
+    <div className="footer-container" style={{ background: "var(--dark-background, #181818)", color: "white", marginTop: "50px" }}>
       <div className="footer-column">
         <h3>Fingertipe</h3>
         <ul className="no-list-style">
@@ -16,7 +17,7 @@ const Footer = () => {
       <div className="footer-column">
         <h3>Resource</h3>
         <ul className="no-list-style">
-           <li>Home</li>
+          <li>Home</li>
           <li>Examples</li>
           <li>Pricing</li>
           <li>Updates</li>
@@ -33,21 +34,26 @@ const Footer = () => {
       </div>
       <div className='contact-info'>
         <div className='info-item'>
-        <img src='bx_bx-map (1).png' alt=''/>
-        <p>Ul. Vjekoslava Heinzela 40,
-        10000 Zagreb, Hrvatska</p>
+          <img src='bx_bx-map (1).png' alt='' />
+          <p>Ul. Vjekoslava Heinzela 40, 10000 Zagreb, Hrvatska</p>
         </div>
         <div className='info-item'>
-        <img src='ic_baseline-phone-android.png' alt=''/>
-        <p>(239) 555-0108</p>
+          <img src='ic_baseline-phone-android.png' alt='' />
+          <p>(239) 555-0108</p>
         </div>
         <div>
-        <img src='ant-design_twitter-outlined.png' id='footer-img' alt=''/>
-        <img src='ant-design_facebook-filled.png' id='footer-img' alt=''/>
-        <img src='ant-design_linkedin-filled.png' id='footer-img' alt=''/>
+          <img src='ant-design_twitter-outlined.png' id='footer-img' alt='' />
+          <img src='ant-design_facebook-filled.png' id='footer-img' alt='' />
+          <img src='ant-design_linkedin-filled.png' id='footer-img' alt='' />
         </div>
       </div>
+
+    
     </div>
+      <div className='copy'>
+      <p className='copy-p'>© 2023 BeeHiveSG. All rights reserved.</p>
+    </div>
+    </section>
   );
 };
 
